207fd42ac4930219f2a9d42528113c8d53957e76
Sean Little <seanchristopherlittle@gmail.com>
2022-04-09 19:14:06 -0500

    MyNameIs

---
+++ Week1/Extra Practice/MyNameIs.java
@@ -0,0 +1,7 @@
+public class MyNameIs {
+    public static void main(String[] args) {
+        System.out.println("My name is Sean Christopher Little");
+        System.out.println("I am 40 years old");
+        System.out.println("My hometown is Old Saybrook, CT");
+    }
+}
