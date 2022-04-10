91c14661cc41e6099f7612524553f396ce084efe
Sean Little <seanchristopherlittle@gmail.com>
2022-04-09 21:16:34 -0500

    CafeJava

--- Week1/Extra Practice/CafeJava.java
+++ Week1/Extra Practice/CafeJava.java
@@ -3,9 +3,11 @@ public class CafeJava {
         // APP VARIABLES
         // Lines of text that will appear in the app. 
         String generalGreeting = "Welcome to Cafe Java, ";
-        String pendingMessage = ", your order will be ready shortly";
-        String readyMessage = ", your order is ready";
+        String pendingMessage = "Your order will be ready shortly, ";
+        String readyMessage = "Your order is ready, ";
         String displayTotalMessage = "Your total is $";
+        String mistakeMessage = "Apolgies for the mistake, ";
+        String newTotalMessage = "Your the difference will be $";
         
         // Menu variables 
         double mochaPrice = 3.5;
@@ -29,10 +31,13 @@ public class CafeJava {
         // Order completions (add yours below)
         boolean isReadyOrder1 = false;
         boolean isReadyOrder2 = true;
+        boolean isCorrectOrder = false;
     
         // APP INTERACTION SIMULATION (Add your code for the challenges below)
         // Example:
         System.out.println(generalGreeting + customer1); // Displays "Welcome to Cafe Java, Cindhuri"
+        System.out.println(readyMessage + customer1)
+        System.out.println(displayTotalMessage + dripCoffeePrice)
     	// ** Your customer interaction print statements will go here ** //
         
         System.out.println(generalGreeting + customer2);
@@ -52,5 +57,45 @@ public class CafeJava {
         else {
             System.out.println(pendingMessage + customer3);
         }
+
+        //Noah ordered a cappucino. 
+        //Use an if statement to check the status of his order and print the correct status message. 
+        //If it is ready, also print the message to display the total. 
+        System.out.println(generalGreeting + customer4);
+        if (isReadyOrder1) {
+            System.out.println(pendingMessage + customer4);
+        }
+        else {
+            System.out.println(readyMessage + customer3);
+            System.out.println(displayTotalMessage + (DripCoffeePrice + LattePrice));
+        }
+
+        //Sam just ordered 2 lattes, print the message to display their total.
+        //Next, use an if statement to print the appropriate order status message. 
+        //Change the isReady flag value from true to false or vice versa in order to test your control logic (if-statement).
+        System.out.println(generalGreeting + customer2);
+        if (isReadyOrder2) {
+            System.out.println(readyMessage + customer2);
+            System.out.println(displayTotalMessage + (LattePrice * 2));
+        }
+        else {
+            System.out.println(pendingMessage + customer2);
+        }
+
+        ///Jimmy just realized he was charged for a coffee but had ordered a latte. 
+        ///Print the total message with the new calculated total (what he owes) to make up the difference.
+        System.out.println(generalGreeting + customer3);
+        if (isCorrectOrder) {
+            System.out.println(mistakeMessage + customer2);
+            System.out.println(newTotalMessage + (LattePrice - dripCoffeePrice));
+        }
+        else if (isReadyOrder1) {
+            System.out.println(pendingMessage + customer2);
+        }
+        else {
+            System.out.println(readyMessage + customer2;)
+        }
+
     }
+
 }
